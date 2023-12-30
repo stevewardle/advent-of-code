e2c95df541372781446e2aa56708aa74e4e0b9ed
stevewardle <steve_wardle@hotmail.co.uk>
2023-12-30 21:32:48 +0000

    2023 - Day 22 - Part 2

--- 2023/day-22/slabs.f90
+++ 2023/day-22/slabs.f90
@@ -11,23 +11,25 @@ PROGRAM day22
   TYPE(slab), &
     ALLOCATABLE         :: slabs(:)
   LOGICAL, &
-    ALLOCATABLE         :: mask(:), footprint(:,:)
+    ALLOCATABLE         :: mask(:), footprint(:,:), safe(:), falls(:)
   INTEGER(KIND=int32), &
     ALLOCATABLE         :: order(:), max_z(:,:), filled(:,:), &
                            supports(:,:), supported(:,:)
-  LOGICAL               :: safe 
   INTEGER(KIND=int32)   :: i, j, k, l, grid_x, grid_y, new_z, &
                            max_support_map, total
 
 
   CALL read_input("input.txt", slabs)
 
-  ! Find extent of "grid"
+  ! Find extent of the grid in each direction
   grid_x = MAX(MAXVAL(slabs(:)%x1), MAXVAL(slabs(:)%x2))
   grid_y = MAX(MAXVAL(slabs(:)%y1), MAXVAL(slabs(:)%y2))
+  ! The maximum number of support/supported relations for
+  ! a block - this can't be more than the longest dimension
+  ! of the grid in the worst case
   max_support_map = MAX(grid_x, grid_y) + 1
   
-  ! Iterate in lowest Z order
+  ! Prepare an iterator which goes in ascending Z order
   ALLOCATE(mask(SIZE(slabs)))
   ALLOCATE(order(SIZE(slabs)))
   mask = .TRUE.
@@ -36,8 +38,10 @@ PROGRAM day22
       MINLOC(slabs(:)%z1 + slabs(:)%z2, DIM=1, MASK=mask)
     mask(order(i)) = .FALSE. 
   END DO
+  DEALLOCATE(mask)
 
-  ! Figure out where each piece will fit
+  ! Place slabs and calculate which slabs support
+  ! each other
   ALLOCATE(max_z(0:grid_x, 0:grid_y))
   ALLOCATE(footprint(0:grid_x, 0:grid_y))
   ALLOCATE(filled(0:grid_x, 0:grid_y))
@@ -48,6 +52,8 @@ PROGRAM day22
   supports(:,:) = 0
   supported(:,:) = 0
   DO i=1,SIZE(slabs)
+    ! Create a 2d footprint for the slab which acts
+    ! like a collision mask
     footprint(:,:) = .FALSE.
     footprint(slabs(order(i))%x1:slabs(order(i))%x2, &
               slabs(order(i))%y1:slabs(order(i))%y2) = .TRUE.
@@ -61,9 +67,14 @@ PROGRAM day22
     ! other slabs (and vice versa)
     DO j=0,grid_x
       DO k=0,grid_y
+        ! Filter to points within the slab footprint that
+        ! are at the previous max height and are not the
+        ! ground
         IF (footprint(j,k) &
             .AND. (max_z(j,k) == new_z-1) &
             .AND. (filled(j,k) > 0)) THEN
+          ! Populate the list of slabs which the current
+          ! slab is supported by
           DO l=1,max_support_map
             IF (supported(order(i),l) == filled(j,k)) EXIT
             IF (supported(order(i),l) == 0) THEN
@@ -71,6 +82,8 @@ PROGRAM day22
               EXIT
             END IF
           END DO
+          ! Populate the reverse lists of each slab which
+          ! supports the current slab
           DO l=1,max_support_map
             IF (supports(filled(j,k),l) == order(i)) EXIT
             IF (supports(filled(j,k),l) == 0) THEN
@@ -82,43 +95,73 @@ PROGRAM day22
       END DO
     END DO
 
-    ! Update the Z array by the Z size of the
+    ! Update the Z array by the maximum Z of the
     ! given piece within its footprint, and the
     ! filled array to indicate the id of each slab
+    ! for use in the calculations above
     WHERE(footprint)
       max_z = &
         new_z &
         + ABS(slabs(order(i))%z1 - slabs(order(i))%z2)
       filled = order(i)
     END WHERE
-
   END DO
-
-  ! Can now check for disintegrations
-  total = 0
-  DO i=1,SIZE(slabs)
-    safe = .TRUE.
-    DO j=1,max_support_map
-      IF (supports(i,j) == 0) EXIT
-      IF (COUNT(supported(supports(i,j),:) > 0) == 1) THEN
-        safe = .FALSE.
-        EXIT
-      END IF
-    END DO
-    IF (safe) THEN
-      total = total + 1
-    END IF
-  END DO
-
-  WRITE(*, "(A,I0)") "Slabs that can be disintegrated: ", total
-
-  DEALLOCATE(supported)
-  DEALLOCATE(supports)
   DEALLOCATE(filled)
   DEALLOCATE(footprint)
   DEALLOCATE(max_z)
+
+  ! Can now check for disintegration safety
+  total = 0
+  ALLOCATE(safe(SIZE(slabs)))
+  DO i=1,SIZE(slabs)
+    safe(i) = .TRUE.
+    DO j=1,max_support_map
+      IF (supports(i,j) == 0) EXIT
+      IF (COUNT(supported(supports(i,j),:) > 0) == 1) THEN
+        safe(i) = .FALSE.
+        EXIT
+      END IF
+    END DO
+  END DO
+
+  WRITE(*, "(A,I0)") "Slabs that can be disintegrated: ", COUNT(safe) 
+
+  ! Check for chain reactions
+  ALLOCATE(falls(SIZE(slabs)))
+  total = 0
+  DO i=1,SIZE(slabs)
+    ! Can use our part 1 solution to skip any that we know
+    ! won't cause any other blocks to fall
+    IF (.NOT. safe(order(i))) THEN
+      falls(:) = .FALSE.
+      falls(order(i)) = .TRUE.
+      DO j=i+1,SIZE(slabs)
+        ! Ignore any that aren't supported by any other blocks
+        IF (supported(order(j),1) /= 0) THEN
+          ! Assume the block will fall, check if each block
+          ! which supports it is falling and if any are not
+          ! then it does not fall
+          falls(order(j)) = .TRUE.
+          DO k=1,max_support_map
+            IF (supported(order(j),k) == 0) EXIT
+            IF (.NOT. falls(supported(order(j),k))) THEN
+              falls(order(j)) = .FALSE.
+              EXIT
+            END IF
+          END DO
+        END IF
+      END DO
+      total = total + COUNT(falls)-1
+    END IF
+  END DO
+
+  WRITE(*, "(A,I0)") "Sum of Slabs that would fall: ", total
+
+  DEALLOCATE(falls)
+  DEALLOCATE(safe)
+  DEALLOCATE(supported)
+  DEALLOCATE(supports)
   DEALLOCATE(order)
-  DEALLOCATE(mask)
   DEALLOCATE(slabs)
 
   CONTAINS
