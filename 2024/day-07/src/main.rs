09c85b80bb9ea2fea4bcf387b2942a514c8d7d06
Steve Wardle <steve_wardle@hotmail.co.uk>
2024-12-18 14:27:57 +0000

    2024 Day 7 part 2

--- 2024/day-07/src/main.rs
+++ 2024/day-07/src/main.rs
@@ -9,21 +9,33 @@ fn main() -> Result<()> {
 
     let mut total_calibration = 0;
     for equation in equations.iter() {
-        if check_equation(&equation)? {
+        if check_equation(&equation, false)? {
             total_calibration += equation.test_value;
         }
     }
 
     println!("Total Calibration Result: {}", total_calibration);
+
+    total_calibration = 0;
+    for equation in equations.iter() {
+        if check_equation(&equation, true)? {
+            total_calibration += equation.test_value;
+        }
+    }
+
+    println!("Total Calibration Result (with Concatenation): {}", total_calibration);
     Ok(())
 }
 
-fn check_equation(equation:&Equation) -> Result<bool> {
+fn check_equation(equation:&Equation, concat:bool) -> Result<bool> {
 
+    // If the running result has grown too big we are done
     if equation.numbers[0] > equation.test_value {
         return Ok(false)
     }
+    // If we have reached the final total
     if equation.numbers.len() == 1 {
+        // And matched the test value - we've found a valid result
         if equation.numbers[0] == equation.test_value {
             return Ok(true)
         } else {
@@ -34,15 +46,25 @@ fn check_equation(equation:&Equation) -> Result<bool> {
     let mut add_next_equation = equation.clone();
     add_next_equation.numbers[0] = add_next_equation.numbers[0] + add_next_equation.numbers[1];
     add_next_equation.numbers.remove(1);
-    if check_equation(&add_next_equation)? {
+    if check_equation(&add_next_equation, concat)? {
         return Ok(true)
     }
 
     let mut multiply_next_equation = equation.clone();
     multiply_next_equation.numbers[0] = multiply_next_equation.numbers[0] * multiply_next_equation.numbers[1];
     multiply_next_equation.numbers.remove(1);
-    if check_equation(&multiply_next_equation)? {
+    if check_equation(&multiply_next_equation, concat)? {
         return Ok(true)
+    }
+
+    if concat {
+        let mut concat_next_equation = equation.clone();
+        let concatenated = format!("{}{}", concat_next_equation.numbers[0], concat_next_equation.numbers[1]);
+        concat_next_equation.numbers[0] = concatenated.parse::<i64>()?;
+        concat_next_equation.numbers.remove(1);
+        if check_equation(&concat_next_equation, concat)? {
+            return Ok(true)
+        }
     }
 
     Ok(false)
