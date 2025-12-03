9f8a73afa4597adb226bd495ec51122a0cd56c9a
Steve Wardle <steve_wardle@hotmail.co.uk>
2025-12-03 07:49:09 +0000

    2025 Day 3 Part 2

--- 2025/day-03/main.py
+++ 2025/day-03/main.py
@@ -9,29 +9,34 @@ def read_input(file):
     return banks
 
 
-def calc_joltage_p1(bank):
+def calc_joltage(bank, battery_count=2):
     print(''.join(map(str, bank)))
-    max_battery = max(bank)
-    max_battery_index = bank.index(max_battery)
-    bank_right = bank[max_battery_index + 1:]
-    if bank_right:
-        max_battery_right = max(bank_right)
-        joltage = int(f"{max_battery}{max_battery_right}")
-    else:
-        bank_left = bank[:max_battery_index]
-        max_battery_left = max(bank_left)
-        joltage = int(f"{max_battery_left}{max_battery}")
+    batteries = []
+    bank_search = bank
+    while len(batteries) < battery_count:
+        # Find the largest battery that still leaves enough available
+        # further in the bank
+        upper_bound = len(bank_search) - (battery_count - len(batteries)) + 1
+        max_battery = max(bank_search[:upper_bound])
+        max_battery_index = bank_search.index(max_battery)
+        # Cut the search space to only after the found battery
+        bank_search = bank_search[max_battery_index + 1:]
+        batteries.append(max_battery)
+    joltage = int("".join(map(str, batteries)))
     return joltage
 
 
 def main(input_file):
     banks = read_input(input_file)
-    total_output_joltage = 0
+    total_output_joltage_p1 = 0
+    total_output_joltage_p2 = 0
     for bank in banks:
-        joltage = calc_joltage_p1(bank)
-        print(f"Joltage: {joltage}")
-        total_output_joltage += joltage
-    print(f"Total Output Joltage: {total_output_joltage}")
+        joltage_p1 = calc_joltage(bank)
+        joltage_p2 = calc_joltage(bank, battery_count=12)
+        total_output_joltage_p1 += joltage_p1
+        total_output_joltage_p2 += joltage_p2
+    print(f"Total Output Joltage (Part 1): {total_output_joltage_p1}")
+    print(f"Total Output Joltage (Part 2): {total_output_joltage_p2}")
 
 
 if __name__ == "__main__":
